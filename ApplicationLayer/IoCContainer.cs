ca5af329f2773e0da365c2bde45d014d7c47d6c0
AzureDevOpsTeam <azurepanel@yahoo.com>
2025-09-06 22:10:14 +0330

    Change Cros Origin

--- ApplicationLayer/IoCContainer.cs
+++ ApplicationLayer/IoCContainer.cs
@@ -39,9 +39,11 @@ namespace ApplicationLayer
             services.RateLimitingConfiguration(configuration);
             services.AddCors(opt => opt.AddPolicy("AllowSpecificOrigin", builder =>
             {
-                builder.AllowAnyOrigin()
+                builder.WithOrigins("https://tg.packsi.net", "https://api.packsi.net", "https://panel.packsi.net", "http://localhost:3000", "http://localhost:5173", "http://localhost:8080", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "http://127.0.0.1:8080")
+                .SetIsOriginAllowed(origin => true) // Allow any origin for development
                 .AllowAnyMethod()
-                .AllowAnyHeader();
+                .AllowAnyHeader()
+                .AllowCredentials();
             }));
 
             services.AddHsts(options =>
