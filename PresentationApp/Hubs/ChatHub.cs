ac8575854c4d6669b1dce898582de67b0dc4f9ee
shahr <shahramoweisy@gmail.com>
2025-09-07 08:28:08 +0330

    Sample

--- PresentationApp/Hubs/ChatHub.cs
+++ PresentationApp/Hubs/ChatHub.cs
@@ -9,6 +9,11 @@ namespace PresentationApp.Hubs;
 
 public class ChatHub : Hub
 {
+    public override async Task OnConnectedAsync()
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, $"User_{13}");
+        await base.OnConnectedAsync();
+    }
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
