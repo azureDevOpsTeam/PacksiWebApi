73ac748a8bd2b131f660885f0399c152e473e3ec
AzureDevOpsTeam <azurepanel@yahoo.com>
2025-09-12 22:11:33 +0330

    Build

--- ApplicationLayer/Extensions/SmartEnums/TransportableItemTypeEnum.cs
+++ ApplicationLayer/Extensions/SmartEnums/TransportableItemTypeEnum.cs
@@ -5,8 +5,9 @@ namespace ApplicationLayer.Extensions.SmartEnums;
 public class TransportableItemTypeEnum : SmartEnum<TransportableItemTypeEnum>
 {
     public static readonly TransportableItemTypeEnum Document = new(1, "مدرک", nameof(Document));
-    public static readonly TransportableItemTypeEnum Pet = new(2, "پت", nameof(Pet));
-    public static readonly TransportableItemTypeEnum Medicine = new(3, "دارو", nameof(Medicine));
+    public static readonly TransportableItemTypeEnum Cat = new(2, "پیشی", nameof(Cat));
+    public static readonly TransportableItemTypeEnum Dog = new(3, "هاپو", nameof(Dog));
+    public static readonly TransportableItemTypeEnum Medicine = new(4, "دارو", nameof(Medicine));
 
     public string PersianName { get; }
 
