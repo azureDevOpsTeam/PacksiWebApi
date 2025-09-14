2c2d9b85f90dab7ee3aa2598ef7e422346676b8c
AzureDevOpsTeam <azurepanel@yahoo.com>
2025-09-14 23:21:23 +0330

    build

--- ApplicationLayer/DTOs/MiniApp/FlightDto.cs
+++ ApplicationLayer/DTOs/MiniApp/FlightDto.cs
@@ -82,7 +82,7 @@ public class TripsDto
                     : "btnDisable";
             else
             {
-                return "btnDisable";
+                return "btnNotFound";
             }
         }
     }
@@ -100,7 +100,7 @@ public class TripsDto
             }
             else
             {
-                return "btnDisable";
+                return "btnNotFound";
             }
         }
     }
