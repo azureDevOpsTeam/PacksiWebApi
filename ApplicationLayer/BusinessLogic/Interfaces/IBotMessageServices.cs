94cb1c6034bae81f5d7cdced791ccbb97236e5a5
shahr <shahramoweisy@gmail.com>
2025-10-01 16:19:22 +0330

    fixed bug

--- ApplicationLayer/BusinessLogic/Interfaces/IBotMessageServices.cs
+++ ApplicationLayer/BusinessLogic/Interfaces/IBotMessageServices.cs
@@ -8,5 +8,5 @@ public interface IBotMessageServices
     Task<Result<bool>> DepartureCountriesAsync(long telegramUserId);
     Task<Result<bool>> PreferredCountriesAsync(long telegramUserId);
     Task<Result<bool>> SendWelcomeMessageAsync(RegisterReferralDto Model);
-    Task<Result<bool>> StepTwoAsync(RegisterReferralDto model);
+    Task<Result<bool>> StepTwoAsync(long telegramUserId);
 }
